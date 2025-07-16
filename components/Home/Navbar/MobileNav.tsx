0f315d7afd682847b40c91811fbe5232a9ec4e2b
S M Masrafi <smmasrafi000@gmail.com>
2025-07-17 01:28:56 +0600

    Updated

---
+++ components/Home/Navbar/MobileNav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MobileNav = () => {
+  return (
+    <div>
+      {/* Overlay */}
+      <div className='fixed inset-0 transform transition-all duration-500 z-[1002] bg-black opacity-70 w-full h-svh'></div>
+    </div>
+  )
+}
+
+export default MobileNav
---
+++ constants/constant.ts
@@ -0,0 +1,32 @@
+export const Navlinks = [
+    {
+        id: "1",
+        url: "#",
+        title: "Home",
+    },
+    {
+        id: "2",
+        url: "#",
+        title: "Company",
+    },
+    {
+        id: "3",
+        url: "#",
+        title: "Services",
+    },
+    {
+        id: "4",
+        url: "#",
+        title: "Portfolio",
+    },
+    {
+        id: "5",
+        url: "#",
+        title: "Blog",
+    },
+    {
+        id: "6",
+        url: "#",
+        title: "Contact",
+    },
+]
